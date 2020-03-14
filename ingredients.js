7be103c8ee49bb2bfae3c6c2b1fd9c18c0167169
Stacey Keating <staceyamberkk@gmail.com>
2020-03-14 05:37:16 +0000

    forward backward for while arrays

---
+++ ingredients.js
@@ -0,0 +1,16 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+let i = 0;
+
+while (i < ingredients.length) 
+  console.log(ingredients[i]);
+  i++;
+
+
+for (let i = 0; i < ingredients.length; i++) 
+  console.log(ingredients[i]);
+
+
+  for (var x = 7; x >= 0; x--)
+  console.log(ingredients[x]);
+
